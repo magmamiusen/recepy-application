a4c2b7591c87bb238944f4f09329ec5febfdc50e
pablo <hamburguesasinsalsa@gmail.com>
2021-10-04 14:31:11 -0500

    build monday

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Juan Pablo Gutierrez Gutierrez
         </h1>
 
         <p className={styles.description}>
